cd52258b309737a828dbd5fb147873b2cbcee01e
Chris Couzens <ccouzens@gmail.com>
2020-06-27 13:02:55 +0100

    Support complex types with complex content

--- transformer/src/parsers/doc/etc/complex_type.rs
+++ transformer/src/parsers/doc/etc/complex_type.rs
@@ -25,6 +25,7 @@ pub(super) fn parse_complex_type(input: &xmltree::XMLNode) -> Option<ComplexType
             let name = attributes.get("name")?.clone();
             let annotation = children.iter().filter_map(parse_annotation).next()?;
             let mut sequence_elements = Vec::new();
+            let mut parent = None;
 
             for child in children {
                 match child {
@@ -37,6 +38,49 @@ pub(super) fn parse_complex_type(input: &xmltree::XMLNode) -> Option<ComplexType
                         sequence_elements
                             .extend(children.iter().filter_map(parse_sequence_element));
                     }
+                    xmltree::XMLNode::Element(xmltree::Element {
+                        namespace: Some(namespace),
+                        name,
+                        children,
+                        ..
+                    }) if namespace == XML_SCHEMA_NS && name == "complexContent" => {
+                        for child in children {
+                            match child {
+                                xmltree::XMLNode::Element(xmltree::Element {
+                                    attributes,
+                                    namespace: Some(namespace),
+                                    name,
+                                    children,
+                                    ..
+                                }) if namespace == XML_SCHEMA_NS && name == "extension" => {
+                                    parent = attributes.get("base").cloned();
+                                    for child in children {
+                                        match child {
+                                            xmltree::XMLNode::Element(xmltree::Element {
+                                                namespace: Some(namespace),
+                                                name,
+                                                children,
+                                                ..
+                                            }) if namespace == XML_SCHEMA_NS
+                                                && name == "sequence" =>
+                                            {
+                                                sequence_elements.extend(
+                                                    children
+                                                        .iter()
+                                                        .filter_map(parse_sequence_element),
+                                                );
+                                            }
+                                            _ => {}
+                                        }
+                                    }
+                                    sequence_elements
+                                        .extend(children.iter().filter_map(parse_sequence_element));
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+
                     _ => {}
                 }
             }
@@ -45,7 +89,7 @@ pub(super) fn parse_complex_type(input: &xmltree::XMLNode) -> Option<ComplexType
                 name,
                 annotation,
                 sequence_elements,
-                parent: None,
+                parent,
             })
         }
         _ => None,
@@ -101,6 +145,87 @@ fn parse_base_type_test() {
                 })
             }],
             parent: None
+        })
+    );
+}
+
+#[test]
+fn parse_type_wth_parent_test() {
+    let xml: &[u8] = br#"
+    <xs:complexType xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:meta="http://www.vmware.com/vcloud/meta" name="TestType">
+        <xs:annotation>
+            <xs:appinfo>
+                <meta:content-type>application/vnd.ccouzens.test</meta:content-type>
+            </xs:appinfo>
+            <xs:documentation source="since">0.9</xs:documentation>
+            <xs:documentation xml:lang="en">
+                A simple type to test the parser
+            </xs:documentation>
+        </xs:annotation>
+        <xs:complexContent>
+            <xs:extension base="BaseType">
+                <xs:sequence>
+                    <xs:element name="OptionalString" type="xs:string" minOccurs="0">
+                        <xs:annotation>
+                            <xs:documentation source="modifiable">always</xs:documentation>
+                            <xs:documentation xml:lang="en">
+                                String that may or may not be here
+                            </xs:documentation>
+                            <xs:documentation source="required">false</xs:documentation>
+                        </xs:annotation>
+                    </xs:element>
+                    <xs:element name="RequiredString" type="xs:string" minOccurs="1">
+                        <xs:annotation>
+                            <xs:documentation source="modifiable">always</xs:documentation>
+                            <xs:documentation xml:lang="en">
+                                String that will be here
+                            </xs:documentation>
+                            <xs:documentation source="required">true</xs:documentation>
+                        </xs:annotation>
+                    </xs:element>
+                </xs:sequence>
+            </xs:extension>
+        </xs:complexContent>
+    </xs:complexType>    "#;
+    let tree = xmltree::Element::parse(xml).unwrap();
+    assert_eq!(
+        parse_complex_type(&xmltree::XMLNode::Element(tree)),
+        Some(ComplexType {
+            annotation: Annotation {
+                description: "A simple type to test the parser".to_owned(),
+                required: None,
+                deprecated: false,
+                modifiable: None,
+                content_type: Some("application/vnd.ccouzens.test".to_owned())
+            },
+            name: "TestType".to_owned(),
+            sequence_elements: vec![
+                SequenceElement {
+                    annotation: Some(Annotation {
+                        description: "String that may or may not be here".to_owned(),
+                        required: Some(false),
+                        deprecated: false,
+                        modifiable: Some(Modifiable::Always),
+                        content_type: None
+                    }),
+                    name: "optionalString".to_owned(),
+                    r#type: "xs:string".to_owned(),
+                    occurrences: Occurrences::Optional
+                },
+                SequenceElement {
+                    annotation: Some(Annotation {
+                        description: "String that will be here".to_owned(),
+                        required: Some(true),
+                        deprecated: false,
+                        modifiable: Some(Modifiable::Always),
+                        content_type: None
+                    }),
+                    name: "requiredString".to_string(),
+                    r#type: "xs:string".to_owned(),
+                    occurrences: Occurrences::One
+                }
+            ],
+            parent: Some("BaseType".to_owned())
         })
     );
 }
