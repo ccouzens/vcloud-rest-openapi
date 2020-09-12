3552045ff474df147e5adabb250bc7d6a42693a0
Chris Couzens <ccouzens@gmail.com>
2020-09-12 22:46:21 +0100

    Better type Metadata
    The documentation does not handle subtypes/sum types well, and requires
    manual intervention.

--- transformer/src/schemas.rs
+++ transformer/src/schemas.rs
@@ -97,6 +97,99 @@ pub fn schemas<R: Read + Seek>(
         }),
     );
 
+    let metadata_types: Vec<(String, String)> = output
+        .keys()
+        .filter_map(|k| {
+            k.strip_prefix("vcloud_").and_then(|t| {
+                if t.starts_with("Metadata") && t.ends_with("Value") && t != "MetadataTypedValue" {
+                    Some((String::from(k), String::from(t)))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect();
+
+    output.insert(
+        String::from("MetadataTypedValue"),
+        ReferenceOr::Item(Schema {
+            schema_kind: openapiv3::SchemaKind::OneOf {
+                one_of: metadata_types
+                    .iter()
+                    .map(|qrt| openapiv3::ReferenceOr::Reference {
+                        reference: qrt.0.clone(),
+                    })
+                    .collect(),
+            },
+            schema_data: openapiv3::SchemaData {
+                discriminator: Some(openapiv3::Discriminator {
+                    property_name: String::from("_type"),
+                    mapping: metadata_types
+                        .iter()
+                        .map(|qrt| (qrt.1.clone(), format!("#/components/schemas/{}", qrt.0)))
+                        .collect(),
+                }),
+                ..Default::default()
+            },
+        }),
+    );
+
+    for schema in output.values_mut() {
+        if let openapiv3::ReferenceOr::Item(openapiv3::Schema {
+            schema_kind: openapiv3::SchemaKind::AllOf { all_of },
+            ..
+        }) = schema
+        {
+            if let Some(properties) = all_of.iter_mut().find_map(|ref_schema| match ref_schema {
+                ReferenceOr::Item(openapiv3::Schema {
+                    schema_kind:
+                        openapiv3::SchemaKind::Type(openapiv3::Type::Object(openapiv3::ObjectType {
+                            properties,
+                            ..
+                        })),
+                    ..
+                }) => Some(properties),
+                _ => None,
+            }) {
+                if let Some(openapiv3::ReferenceOr::Item(typed_value)) =
+                    properties.get_mut("typedValue")
+                {
+                    typed_value.schema_kind = openapiv3::SchemaKind::AllOf {
+                        all_of: vec![openapiv3::ReferenceOr::Reference {
+                            reference: String::from("#/components/schemas/MetadataTypedValue"),
+                        }],
+                    }
+                }
+            };
+        };
+    }
+
+    if let Some(openapiv3::ReferenceOr::Item(openapiv3::Schema {
+        schema_kind:
+            openapiv3::SchemaKind::Type(openapiv3::Type::Object(openapiv3::ObjectType {
+                properties,
+                required,
+                ..
+            })),
+        ..
+    })) = output.get_mut("vcloud_MetadataTypedValue")
+    {
+        properties.entry(String::from("_type")).or_insert_with(|| {
+            openapiv3::ReferenceOr::boxed_item(openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(openapiv3::Type::String(
+                    openapiv3::StringType {
+                        enumeration: metadata_types.into_iter().map(|qrt| qrt.1).collect(),
+                        ..Default::default()
+                    },
+                )),
+            })
+        });
+        if required.iter().find(|&r| r == "_type") == None {
+            required.push(String::from("_type"));
+        }
+    }
+
     let query_record_types: Vec<(String, String)> = output
         .keys()
         .filter_map(|k| {
@@ -112,6 +205,7 @@ pub fn schemas<R: Read + Seek>(
             })
         })
         .collect();
+
     output.insert(
         String::from("QueryResultRecordType"),
         ReferenceOr::Item(Schema {
